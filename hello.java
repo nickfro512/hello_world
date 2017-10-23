4d3b55d8b2c63d3edbeaba65ba9ff40dbb8b7574
nickfro512 <nickfro@gmail.com>
2017-10-23 16:58:47 -0400

    Added input

--- hello.java
+++ hello.java
@@ -1,16 +1,18 @@
 // a simple program to practice common Github commands 
 
+import java.util.Scanner; 
+
 public class hello {
 
     public static void main(String [] args) {
+		
+		private static Scanner scanner = new Scanner( System.in );
+		
+        System.out.print( "Enter your name" );
 
+        String input = scanner.nextLine();
 
-	// added  line	
-
-        System.out.println("Hello Class!");
-
-
-
+        System.out.println( "Hello " + input + ", my name is Nick" );
     }
 }
 
